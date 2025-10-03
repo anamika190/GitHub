e9f4fc66763f1085486451975b4e54047584fbe7
anamika190 <anamikavinodnair@gmail.com>
2025-10-03 18:51:36 +0530

    Create contact_book.py

---
+++ contact_book.py
@@ -0,0 +1,8 @@
+#input from users
+name=input("enter your name:")
+phone=input("enter your phone number:")
+mail=input("enter your email address:")
+#saved message
+print("contact details saved")
+#printing the input info
+print("name:",name,"phone number:",phone,"email address:", mail)
