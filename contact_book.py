11a109723f084969f770aa3e90717282c9c8959d
anamika190 <anamikavinodnair@gmail.com>
2025-10-06 18:37:11 +0530

    Update contact_book.py

--- contact_book.py
+++ contact_book.py
@@ -1,8 +1,39 @@
+'''#PHASE 1
 #input from users
-name=input("enter your name:")
-phone=input("enter your phone number:")
-mail=input("enter your email address:")
+name=input("enter the name:")
+phone=input("enter the phone number:")
+mail=input("enter the email address:")
 #saved message
 print("contact details saved")
 #printing the input info
-print("name:",name,"phone number:",phone,"email address:", mail)
+print("name:",name,"phone number:",phone,"email address:", mail)'''
+#PHASE 2
+#printing the options 
+contacts = []  # store all contacts
+while True:
+    # printing the options
+    print("\n1. Add Contact")
+    print("2. View All Contacts")
+    print("3. Exit")
+    choice = int(input("Enter your choice: "))
+    # adding contacts
+    if choice == 1:
+        name = input("Enter the name: ")
+        phone = input("Enter the phone number: ")
+        mail = input("Enter the email address: ")
+        contacts.append([name, phone, mail])
+        print("Contact added!")
+    # viewing contacts
+    elif choice == 2:
+        if not contacts:
+            print("No contacts found.")
+        else:
+            print("\nAll Contacts:")
+            for i in contacts:
+                print("Name:", i[0], "Phone:", i[1], "Email:", i[2])
+    # exiting program
+    elif choice == 3:
+        print("Exiting...")
+        break
+    else:
+        print("Error.. enter a valid choice")
